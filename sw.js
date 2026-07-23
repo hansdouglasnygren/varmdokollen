1ee5b79c3f846c66cf94d8c3e6b27f821f1e2989
hansdouglasnygren <hansdouglasnygren@gmail.com>
2026-07-23 14:10:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lonekollen-light-v3';
+const CACHE_NAME = 'lonekollen-light-v4';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
